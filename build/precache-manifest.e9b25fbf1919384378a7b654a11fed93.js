a073335517f9d95a36b10e45b1ad09d0a9edcfde
yudish <yudishchakrawarty3042@gmail.com>
2022-12-23 09:44:48 +0530

    first commit

---
+++ build/precache-manifest.e9b25fbf1919384378a7b654a11fed93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a1cf4b0385f4aa855b13bbdc32a6ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a5d94c35942a8e4268",
+    "url": "/static/css/main.010fcd9a.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a5d94c35942a8e4268",
+    "url": "/static/js/main.22e7359e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
